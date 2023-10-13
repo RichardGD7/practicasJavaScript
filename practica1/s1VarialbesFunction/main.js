7ee698a04a8cf46d9818968fe83f3aefa3c4c1a2
Ricardo Gomez <gduran.ricardo@gmail.com>
2023-10-12 22:26:59 -0600

    feat: add folder with js file

---
+++ practica1/s1VarialbesFunction/main.js
@@ -0,0 +1,10 @@
+console.log("hola koders")
+let a = 4
+let b = 6
+
+let result1 = a + b
+let result2 = a - b
+let result3 = a / b
+let result4 = a * b
+let result5 = a % b
+console.log("Suma es ",result1,"Resta es ",result2,"Division es ",result3,"Multiplicacion es ",result4,"Modulo es ",result5)
