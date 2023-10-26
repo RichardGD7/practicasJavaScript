5c66711794c1d7793ed41bdc553bb7997daef345
Ricardo Gomez <gduran.ricardo@gmail.com>
2023-10-25 20:31:50 -0600

    feat: add practica9 folder

---
+++ practica9/main.js
@@ -0,0 +1,8 @@
+/*FUnciones Flecha*/
+const getTriangleArea = (base, height) => (base * height) / 2;
+  
+const checkIfIsEven = number => number % 2 === 0;
+  
+const getRandomKoder = kodersArray => kodersArray[Math.floor(Math.random() * kodersArray.length)];
+
+  
